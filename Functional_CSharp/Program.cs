0317c6d57e1a5ae4728ddbe029db2d57e8966429
bjron <bintosn@gmail.com>
2017-06-03 01:14:24 +0530

    Add Functional Csharp project to tfs

---
+++ Functional_CSharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Functional_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Func<int, int> f = (x) => x + 2;
+            //int i = f(1);
+            //Console.WriteLine(i);
+            //f = (x) => 2 * x + 1;
+            //i = f(1);
+            //Console.WriteLine(i);
+            Student st1 = new Student(22, 34);
+            Student st2 = new Student(22, 34);
+
+            Console.WriteLine(st1.id > st2.id ? st1.id + " Wins..!" : st2.id + " Wins..!");
+            st1.Display();
+        }
+    }
+}
---
+++ Functional_CSharp/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Functional_CSharp
+{
+    class Student
+    {
+        public int id;
+        public double grade;
+
+        public Student(int id, double grade)
+        {
+            this.id = id;
+            this.grade = grade;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("ID:" + id, "Grade:" + grade);
+        }
+
+        public static Student operator +(Student s1, int num)
+        {
+            return new Student(s1.id + num, s1.grade + num);
+        }
+
+        public static bool operator <(Student s1, Student s2)
+        {
+            return s1.grade < s2.grade;
+        }
+
+        public static bool operator >(Student s1, Student s2)
+        {
+            return s1.grade > s2.grade;
+        }
+    }
+}
